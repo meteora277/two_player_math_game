9d72120df0d432fbcf66fdb8127b00b7d9f14652
meteora277 <justin.s.diaz@gmail.com>
2022-01-31 11:52:07 -0500

    initial commit

---
+++ two_player_math_game.rb
@@ -0,0 +1,14 @@
+# class for Game
+# - creates a new game instance / creates p1 and p2 instances/
+# - manages player turns
+
+
+# class for turn
+# - creates new instance of a turn
+# - houses logic for creating a new math question as well as evaluating answers
+# - manages user inpuits 
+
+# class for player
+# - only really needs to contain life points
+# - contains logic for subtracting player points
+

48be92706d7fddea78d5d6eb285b722623df00f5
meteora277 <justin.s.diaz@gmail.com>
2022-01-31 13:14:12 -0500

    add logic to auto increment player name

--- player.rb
+++ player.rb
@@ -1,7 +1,9 @@
 class Player 
-  attr_accessor :life_points
+  attr_accessor :life_points, :player_name
+  @@player_number = 0
   def initialize
-    @life_points = 3
+    @life_points = 3    
+    @player_name = "player #{@@player_number += 1}"
   end
   def subtract_life 
     @life_points -= 1
